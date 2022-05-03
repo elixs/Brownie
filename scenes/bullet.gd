3ef4b4aa71631f10c36cf0ab78ba41f9ad13fff0
Elías Zelada <elixs.777@gmail.com>
2022-05-03 02:11:11 -0400

    enemy melee attack

---
+++ scenes/bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var SPEED = 300
+
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
+
+
+func _on_body_entered(body: Node):
+	print(Manager.player.velocity)
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage(self)
+
+
+func _physics_process(delta):
+	position += SPEED * transform.x * delta
--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -4,6 +4,8 @@ extends KinematicBody2D
 onready var anim_tree = $AnimationTree
 onready var playback = anim_tree.get("parameters/playback")
 onready var detection_area = $DetectionArea
+onready var attack_area = $AttackArea
+onready var attack_cooldown = $AttackCooldown
 
 var SPEED = 50
 var ACCELERATION = 200
@@ -13,9 +15,12 @@ var _target: Node2D = null
 
 var velocity = Vector2()
 
+export var attacking = false
+
 func _ready():
 	anim_tree.active = true
 	detection_area.connect("body_entered", self, "_on_body_entered")
+	attack_area.connect("body_entered", self, "_on_attack_area_body_entered")
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -23,7 +28,12 @@ func _physics_process(delta):
 	var move_input = 0
 	
 	if _target:
-		move_input = sign(_target.global_position.x - global_position.x)
+		var diff = _target.global_position.x - global_position.x
+		move_input = sign(diff)
+		if attack_cooldown.is_stopped():
+			if abs(diff) < 60:
+				attacking = true
+				attack_cooldown.start()
 	
 	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION)
 	
@@ -31,9 +41,21 @@ func _physics_process(delta):
 	
 	if _target and is_on_floor() and _target.global_position.y < global_position.y - 20:
 		velocity.y = -5 * SPEED
+	
+	if attacking:
+		playback.travel("attack")
+	else:
+		if abs(velocity.x) > 10:
+			playback.travel("walk")
+		else:
+			playback.travel("idle")
 
 func take_damage(instigator: Node2D):
 	velocity = (global_position - instigator.global_position).normalized() * 20 * SPEED
 
 func _on_body_entered(body: Node):
 	_target = body
+
+func _on_attack_area_body_entered(body: Node):
+	if body.has_method("take_damage"):
+		body.take_damage(self)
